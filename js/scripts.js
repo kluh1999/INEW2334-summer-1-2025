6d1355d0adc49a87f4aa3c1cd5f1562c2c54f409
Kayla Williamson Pope <kwmarie86@gmail.com>
2025-06-12 00:19:43 -0500

    week2b

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,35 @@
 /* The code below adds the ages of two siblings*/
 
-let siblingOne= 9;
+/*let siblingOne= 9;
 let siblingTwo= 10;
 let combinedAges = (9+10);
 
-console.log("The combined ages of sibling one and sibling two is " + combinedAges);
+/* console.log("The combined ages of sibling one and sibling two is " + combinedAges); */
 
 
+/*
+var i=0;
+alert(i);
+*/
+
+
+
+for (x=0;x<20;x++) {
+    console.log(x);
+}
+
+
+if (x==500) {
+    console.log("YES, IT IS");
+
+}
+else {
+    console.log("no it is not");
+}
+
+/*
+while (x<40) {
+    x++;
+    console.log("while"+x);
+}
+*/
