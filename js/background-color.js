9a663f67e3cdb393d5c220ff19ee5fe823679105
Kayla Williamson Pope <kwmarie86@gmail.com>
2025-06-21 03:29:41 -0500

    Week 3D

--- js/background-color.js
+++ js/background-color.js
@@ -1,10 +1,26 @@
 //Cookie to change color
-document.cookie = "userPrefColor=pink; expires=Mon, 30 Jun 2025 12:30:00 UTC; path=/";
+//document.cookie = "userPrefColor=pink; expires=Mon, 30 Jun 2025 12:30:00 UTC; path=/";
 
-document.cookie = "lName=Pope; expires=Mon, 30 Jun 2025 12:30:00 UTC; path=/";
+//document.cookie = "lName=Pope; expires=Mon, 30 Jun 2025 12:30:00 UTC; path=/";
+var btn = document.getElementById("btn");
+btn.addEventListener("click", setMyCookie);
+
+var colorPicker = document.getElementById("myColor");
+
+
+//setting the cookie color
+function setMyCookie() {
+    //var userPrefColor = 'magenta';
+    var userPrefColor = colorPicker.value;
+    //alert(userPrefColor);
+    document.cookie = "userPrefColor="+ userPrefColor+" expires=Mon, 30 Jun 2025 12:30:00 UTC; path=/";
+    document.body.style.backgroundColor=userPrefColor;
+    colorPicker.value=userPrefColor;
+}
 
 console.log(document.cookie);
 
+//retrieving cookie values
 function getCookieByName(name){
     const cookies = document.cookie.split(";");
     for(let cookie of cookie) {
@@ -18,4 +34,5 @@ function getCookieByName(name){
 
 // check cookie
 var cookieColorExist = getCookieByName("userPrefColor");
-document.body.style.backgroundColor='purple';
+document.body.style.backgroundColor=cookieColorExist;
+colorPicker.value=cookieColorExist;
